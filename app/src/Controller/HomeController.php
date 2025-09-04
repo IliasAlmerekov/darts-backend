4c41e3f31d5cb3b78d7a71ee2ffcfce152d79cc1
Ilias Teknis <ilias.teknis@db-n.com>
2025-09-04 11:56:38 +0200

    psalm and code sniffer setup

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/')]
-     public function index(): Response
-     {
-         return new Response("hello world");
-     }
+    public function index(): Response
+    {
+        return $this->render(
+            'home/index.html.twig', [
+                'greeting' => 'hello world!',
+            ]
+        );
+    }
 }
